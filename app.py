8399865b6c265cc9fcf2ef73613b9d971a691a94
Tejas Mahajan <tmahajan09876@gmail.com>
2024-02-12 21:50:24 +0530

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 import random
 
@@ -10,6 +10,10 @@ def load_tips_data():
         tips_data = json.load(file)
     return tips_data
 
+@app.route('/', methods=['GET'])
+def home():
+    return "Welcome to Pragmatic Geih API!"
+
 # API endpoint to fetch a random tip
 @app.route('/tips/random', methods=['GET'])
 def get_random_tip():
@@ -17,6 +21,12 @@ def get_random_tip():
     print(tips_data)
     random_tip = random.choice(tips_data)
     return jsonify(random_tip)
+
+
+@app.route('/tips', methods=['GET'])
+def get_all_tips():
+    tips_data = load_tips_data()
+    return jsonify(tips_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
