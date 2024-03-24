f261e4fdfa2b78b83c255cd3c4477be862ba4da8
Tejas Mahajan <59790915+Tejas242@users.noreply.github.com>
2024-03-25 00:14:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS 
 import json
 import random
 
 app = Flask(__name__)
+CORS(app)
 
 # Function to load tips data from JSON file
 def load_tips_data():
